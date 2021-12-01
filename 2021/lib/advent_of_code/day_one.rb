688003560780792dcb4da252c56d135289c80076
Christopher Coleman <christopher.coleman@daveramsey.com>
2021-12-01 09:34:37 -0600

    [2021] Day 1

--- 2021/lib/advent_of_code/day_one.rb
+++ 2021/lib/advent_of_code/day_one.rb
@@ -1,19 +1,33 @@
 module AdventOfCode
   class DayOne
     def self.for(input: $stdin)
-      new(input.readlines)
+      new(input.readlines.map(&:to_i))
     end
 
-    def initialize(lines)
-      @lines = lines
+    attr_reader :depths
+
+    def initialize(depths)
+      @depths = depths
     end
 
     def part_one
-      raise "Not yet!"
+      depths.each_cons(2).reduce(0) do |increases, (previous_depth, current_depth)|
+        if previous_depth < current_depth
+          increases + 1
+        else
+          increases
+        end
+      end
     end
 
     def part_two
-      raise "Not yet!"
+      depths.each_cons(3).each_cons(2).reduce(0) do |increases, (previous_depths, current_depths)|
+        if previous_depths.sum < current_depths.sum
+          increases + 1
+        else
+          increases
+        end
+      end
     end
   end
 end
