3ab1ff1729f8435158b4c26ea1ba94fe652fd797
Christopher Coleman <mellowfish@fastmail.fm>
2021-12-12 15:41:11 -0600

    [2021] Day 12

--- 2021/lib/advent_of_code/day_twelve.rb
+++ 2021/lib/advent_of_code/day_twelve.rb
@@ -1,19 +1,103 @@
 module AdventOfCode
   class DayTwelve
     def self.for(input: $stdin)
-      new(input.readlines)
+      new(CaveSystem.parse(input.readlines.map(&:chomp)))
     end
 
-    def initialize(lines)
-      @lines = lines
+    attr_reader :cave_system
+
+    def initialize(cave_system)
+      @cave_system = cave_system
     end
 
     def part_one
-      raise "Not yet!"
+      cave_system.total_paths(1)
     end
 
     def part_two
-      raise "Not yet!"
+      cave_system.total_paths(2)
+    end
+
+    class CaveSystem < Shared::Model
+      def self.parse(lines)
+        pairs = lines.map { |line| line.split("-") }
+        pairs.reduce(new) do |system, (one, two)|
+          system.add_tunnel(one, two)
+        end
+      end
+
+      attribute :caves, type: Array, default: -> { [] }
+      attribute :tunnels, type: Hash, default: -> { Hash.new { |hash, key| hash[key] = [] } }
+      attribute :small_cave_max, type: Integer, default: 1
+
+      def add_tunnel(one, two)
+        new_caves = caves.dup
+        new_caves |= [one, two]
+        new_tunnels = tunnels.dup
+        new_tunnels[one] << two
+        new_tunnels[two] << one
+
+        with(caves: new_caves, tunnels: new_tunnels)
+      end
+
+      def total_paths(small_cave_max)
+        with(small_cave_max: small_cave_max).paths.size
+      end
+
+      def paths
+        @paths ||= traverse!
+      end
+
+    private
+
+      def traverse!
+        raise "Missing start" unless tunnels.key?("start")
+        raise "Missing end" unless tunnels.key?("end")
+
+        [].tap do |paths|
+          current = "start"
+          partial_path = [current]
+          paths.concat(explore(cave: current, partial_path: partial_path))
+        end.uniq
+      end
+
+      def explore(cave:, partial_path:)
+        # puts "Exploring #{cave}: #{partial_path.join(',')}"
+        return [partial_path] if cave == "end"
+
+        [].tap do |new_paths|
+          tunnels[cave].each do |neighbor|
+            next unless can_visit?(cave: neighbor, partial_path: partial_path)
+
+            new_paths.concat(explore(cave: neighbor, partial_path: partial_path + [neighbor]))
+          end
+        end
+      end
+
+      def small_cave?(cave)
+        cave.downcase == cave
+      end
+
+      def can_visit?(cave:, partial_path:)
+        return true if cave == "end"
+        return false if cave == "start"
+        return true unless small_cave?(cave)
+        return !partial_path.include?(cave) if small_cave_max == 1
+
+        hit_max = partial_path.select { |target| small_cave?(target) }.tally.find do |_target, visits|
+          visits == small_cave_max
+        end
+
+        if hit_max
+          !partial_path.include?(cave)
+        else
+          true
+        end
+      end
+
+      def count_visits(target, path)
+        path.count(target)
+      end
     end
   end
 end
--- 2021/spec/lib/advent_of_code/day_tweleve_spec.rb
+++ 2021/spec/lib/advent_of_code/day_tweleve_spec.rb
@@ -1,33 +1,65 @@
 RSpec.describe AdventOfCode::DayTwelve do
   let(:simple_input) do
     <<~TXT
-      a
-      b
-      c
+      fs-end
+      he-DX
+      fs-he
+      start-DX
+      pj-DX
+      end-zg
+      zg-sl
+      zg-pj
+      pj-he
+      RW-he
+      fs-DX
+      pj-RW
+      zg-RW
+      start-pj
+      he-WI
+      zg-he
+      pj-fs
+      start-RW
     TXT
   end
 
   let(:full_input) do
     <<~TXT
-      a
-      b
-      c
-      d
-      e
-      f
+      yw-MN
+      wn-XB
+      DG-dc
+      MN-wn
+      yw-DG
+      start-dc
+      start-ah
+      MN-start
+      fi-yw
+      XB-fi
+      wn-ah
+      MN-ah
+      MN-dc
+      end-yw
+      fi-end
+      th-fi
+      end-XB
+      dc-XB
+      yw-XN
+      wn-yw
+      dc-ah
+      MN-fi
+      wn-DG
     TXT
   end
 
   describe "#part_one" do
     context "with simple input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(simple_input)).part_one).to eq(1)
+        expect(described_class.for(input: StringIO.new(simple_input)).part_one).to eq(226)
       end
     end
 
     context "with full input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(full_input)).part_one).to eq(42)
+        expect(described_class.for(input: StringIO.new(full_input)).part_one).to eq(4241)
       end
     end
   end
@@ -35,13 +67,13 @@ RSpec.describe AdventOfCode::DayTwelve do
   describe "#part_two" do
     context "with simple input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(simple_input)).part_two).to eq(1)
+        expect(described_class.for(input: StringIO.new(simple_input)).part_two).to eq(3509)
       end
     end
 
     context "with full input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(full_input)).part_two).to eq(42)
+        expect(described_class.for(input: StringIO.new(full_input)).part_two).to eq(122134)
       end
     end
   end
