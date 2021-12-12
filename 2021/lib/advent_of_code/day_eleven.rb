1be3ec10ce208163861847b126e6c4fa0ec86749
Christopher Coleman <mellowfish@fastmail.fm>
2021-12-12 16:28:26 -0600

    [2021] Day 11

--- 2021/lib/advent_of_code/day_eleven.rb
+++ 2021/lib/advent_of_code/day_eleven.rb
@@ -1,19 +1,105 @@
 module AdventOfCode
   class DayEleven
     def self.for(input: $stdin)
-      new(input.readlines)
+      new(FlashingOctopi.parse(input.readlines.map(&:chomp)))
     end
 
-    def initialize(lines)
-      @lines = lines
+    attr_reader :pod
+
+    def initialize(pod)
+      @pod = pod
     end
 
     def part_one
-      raise "Not yet!"
+      100.times.reduce(pod) { |current_pod| current_pod.age }.total_flashes
     end
 
     def part_two
-      raise "Not yet!"
+      current_pod = pod
+      step = 0
+      loop do
+        current_pod = current_pod.age
+        step += 1
+        break if current_pod.max_brightness?
+      end
+      step
+    end
+
+    class FlashingOctopi < Shared::Model
+      def self.parse(lines)
+        new(matrix: lines.map { |line| line.chars.map(&:to_i) }, total_flashes: 0)
+      end
+
+      attribute :matrix, type: Array
+      attribute :total_flashes, type: Integer
+
+      def print
+        puts matrix.map(&:join)
+        puts
+      end
+
+      def age(*)
+        next_total_flashes = total_flashes
+        flashes = []
+        flashes_to_process = []
+        next_matrix = matrix.map(&:dup)
+
+        next_matrix.each_with_index do |octopi, row|
+          octopi.each_with_index do |octopus, column|
+            flashes_to_process << [row, column] if octopus == 9
+            next_matrix[row][column] += 1
+          end
+        end
+
+        until flashes_to_process.empty?
+          flash = flashes_to_process.pop
+          next if flashes.include?(flash)
+
+          flashes << flash
+          neighbors_of(*flash).each do |neighbor_row, neighbor_column|
+            flashes_to_process << [neighbor_row, neighbor_column] if next_matrix[neighbor_row][neighbor_column] == 9
+            next_matrix[neighbor_row][neighbor_column] += 1
+          end
+        end
+
+        flashes.each { |row, column| next_matrix[row][column] = 0 }
+
+        with(
+          matrix: next_matrix,
+          total_flashes: next_total_flashes + flashes.size
+        )
+      end
+
+      def neighbors_of(row, column)
+        [].tap do |neighbors|
+          if row.positive?
+            neighbors << [row - 1, column - 1] if column.positive?
+            neighbors << [row - 1, column]
+            neighbors << [row - 1, column + 1] if column < (width - 1)
+          end
+
+          neighbors << [row, column - 1] if column.positive?
+          neighbors << [row, column + 1] if column < (width - 1)
+
+          if row < (height - 1)
+            neighbors << [row + 1, column - 1] if column.positive?
+            neighbors << [row + 1, column]
+            neighbors << [row + 1, column + 1] if column < (width - 1)
+          end
+        end
+      end
+
+      def height
+        matrix.size
+      end
+
+      def width
+        matrix.first.size
+      end
+
+      def max_brightness?
+        matrix.all? { |octopi| octopi.all?(&:zero?) }
+      end
     end
   end
 end
--- 2021/spec/lib/advent_of_code/day_eleven_spec.rb
+++ 2021/spec/lib/advent_of_code/day_eleven_spec.rb
@@ -1,33 +1,44 @@
 RSpec.describe AdventOfCode::DayEleven do
   let(:simple_input) do
     <<~TXT
-      a
-      b
-      c
+      5483143223
+      2745854711
+      5264556173
+      6141336146
+      6357385478
+      4167524645
+      2176841721
+      6882881134
+      4846848554
+      5283751526
     TXT
   end
 
   let(:full_input) do
     <<~TXT
-      a
-      b
-      c
-      d
-      e
-      f
+    5251578181
+    6158452313
+    1818578571
+    3844615143
+    6857251244
+    2375817613
+    8883514435
+    2321265735
+    2857275182
+    4821156644
     TXT
   end
 
   describe "#part_one" do
     context "with simple input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(simple_input)).part_one).to eq(1)
+        expect(described_class.for(input: StringIO.new(simple_input)).part_one).to eq(1656)
       end
     end
 
     context "with full input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(full_input)).part_one).to eq(42)
+        expect(described_class.for(input: StringIO.new(full_input)).part_one).to eq(1637)
       end
     end
   end
@@ -35,13 +46,13 @@ RSpec.describe AdventOfCode::DayEleven do
   describe "#part_two" do
     context "with simple input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(simple_input)).part_two).to eq(1)
+        expect(described_class.for(input: StringIO.new(simple_input)).part_two).to eq(195)
       end
     end
 
     context "with full input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(full_input)).part_two).to eq(42)
+        expect(described_class.for(input: StringIO.new(full_input)).part_two).to eq(242)
       end
     end
   end
