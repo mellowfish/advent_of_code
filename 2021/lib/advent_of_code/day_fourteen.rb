250d5c30bb6214d7d789563d1e45f769c17f5b84
Christopher Coleman <christopher.coleman@daveramsey.com>
2021-12-14 15:13:54 -0600

    [2021] Day 14 (part 1)

--- 2021/lib/advent_of_code/day_fourteen.rb
+++ 2021/lib/advent_of_code/day_fourteen.rb
@@ -1,19 +1,172 @@
 module AdventOfCode
   class DayFourteen
     def self.for(input: $stdin)
-      new(input.readlines)
+      new(Polymer.parse(input.readlines(chomp: true)))
     end
 
-    def initialize(lines)
-      @lines = lines
+    attr_reader :polymer
+
+    def initialize(polymer)
+      @polymer = polymer
     end
 
     def part_one
-      raise "Not yet!"
+      stack_solution(10)
     end
 
     def part_two
-      raise "Not yet!"
+      stack_solution(15) # 22 takes ~15 seconds, doubles every new depth
+    end
+
+    class Polymer < Shared::Model
+      def self.parse(lines)
+        initial_polymer = lines.shift.chars
+        lines.shift # blank
+        insertion_rules =
+          lines.each_with_object({}) do |line, hash|
+            pair_str, insertion = line.split(" -> ")
+            hash[pair_str.chars] = insertion
+          end
+
+        new(polymer: initial_polymer, insertion_rules: insertion_rules)
+      end
+
+      attribute :polymer, type: Array
+      attribute :insertion_rules, type: Hash
+
+      def grow(*)
+        with(polymer: replacement_for(polymer))
+      end
+
+      def replacement_for(segment)
+        debug = false
+        if insertion_rules.key?(segment)
+          puts "Simple replacement from rules: #{segment.join} => #{insertion_rules[segment].join}" if debug
+          return insertion_rules[segment]
+        end
+
+        segment_size = segment.size
+        if segment_size < 4
+          new_segment = [segment.first]
+          segment.each_cons(2) do |pair|
+            replacement_triplet = insertion_rules[pair]
+            new_segment << replacement_triplet[1]
+            new_segment << replacement_triplet[2]
+          end
+          puts "Pair expansion: #{segment.join} => #{new_segment.join}" if debug
+          insertion_rules[segment] = new_segment
+          return new_segment
+        end
+
+        chunk_size = segment_size / 2
+        chunk_size += 1 if chunk_size * 2 < segment_size
+
+        first_half, second_half = segment.each_slice(chunk_size).to_a
+        puts "About to recurse on half: #{first_half.join}" if debug
+        replaced_first_half = replacement_for(first_half)
+        puts "About to recurse on half: #{second_half.join}" if debug
+        replaced_second_half = replacement_for(second_half)
+        new_segment = replaced_first_half + [replacement_for([first_half.last, second_half.first])[1]] + replaced_second_half
+        puts "Recursive solve: #{segment.join} => #{new_segment.join}" if debug
+        insertion_rules[segment] = new_segment
+        new_segment
+      end
+
+      def score
+        most_common_element_frequency - least_common_element_frequency
+      end
+
+      def tally
+        @tally ||= polymer.tally
+      end
+
+      def most_common_element_frequency
+        tally.values.max
+      end
+
+      def least_common_element_frequency
+        tally.values.min
+      end
+    end
+
+    def stack_solution(depth)
+      debug = false
+
+      tally = Hash.new { |hash, key| hash[key] = 0 }
+      final = []
+      rules = polymer.insertion_rules
+      stack = polymer.polymer.map { |char| [char, depth] }.reverse
+      p stack if debug
+      until stack.empty?
+        first_char, first_depth = stack.pop
+
+        if stack.empty?
+          final << first_char if debug
+          tally[first_char] += 1
+          next
+        end
+
+        if first_depth.zero?
+          final << first_char if debug
+          p final.join if debug
+          tally[first_char] += 1
+          next
+        end
+
+        last_char, last_depth = stack.pop
+        middle_char = rules[[first_char, last_char]]
+        stack << [last_char, last_depth]
+        if first_depth == 1
+          final << first_char if debug
+          final << middle_char if debug
+          tally[middle_char] += 1
+          p final.join if debug
+          tally[first_char] += 1
+        else
+          stack << [middle_char, first_depth - 1]
+          stack << [first_char, first_depth - 1]
+        end
+
+        # puts stack.map(&:join).join
+        p stack if debug
+      end
+      p final.join if debug
+      p tally
+      tally.values.max - tally.values.min
+    end
+
+    def big_array_solution(depth)
+      debug = false
+
+      input = polymer.polymer
+      rules = polymer.insertion_rules
+      size = 2**depth * (input.size - 1) + 1
+      final = Array.new(size)
+
+      input.each_with_index do |char, index|
+        final[index * (size / (input.size - 1))] = char
+      end
+
+      previous_layer_size = input.size
+      (1..depth).each do |layer|
+        puts "Layer: #{depth} (#{Time.now})"
+        puts final.join if debug
+        layer_size = 2**layer * (input.size - 1) + 1
+        num_to_insert = previous_layer_size - 1
+        width = size / num_to_insert
+        p [layer_size, num_to_insert, width] if debug
+        num_to_insert.times do |index|
+          left = index * width
+          right = (index + 1) * width
+          p [left, right] if debug
+          p [final[left], final[right]] if debug
+          final[index * width + (width / 2)] = rules[[final[left], final[right]]]
+        end
+        previous_layer_size = layer_size
+      end
+      puts final.join if debug
+
+      polymer.with(polymer: final).score
     end
   end
 end
--- 2021/spec/lib/advent_of_code/day_fourteen_spec.rb
+++ 2021/spec/lib/advent_of_code/day_fourteen_spec.rb
@@ -1,41 +1,165 @@
 RSpec.describe AdventOfCode::DayFourteen do
   let(:simple_input) do
     <<~TXT
-      a
-      b
-      c
+      NNCB
+      
+      CH -> B
+      HH -> N
+      CB -> H
+      NH -> C
+      HB -> C
+      HC -> B
+      HN -> C
+      NN -> C
+      BH -> H
+      NC -> B
+      NB -> B
+      BN -> B
+      BB -> N
+      BC -> B
+      CC -> N
+      CN -> C
     TXT
   end
+  #  2: N4                                              N4
+  #  3: N3                      C3                      N3
+  #  5: N2          B2          C2          C2          N2
+  #  9: N1    B1    B1    B1    C1    N1    C1    C1    N1
+  # 17: N0 B0 B0 N0 B0 N0 B0 B0 C0 C0 N0 B0 C0 N0 C0 C0 N0
+  #     0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16
+  # size (2): 2^depth + 1
+  # size (n): (n-1) * 2^(depth - n) + 1
 
+  #  4: NNCB
+  #  7: NCNBCHB
+  # 13: NBCCNBBBCBHCB
+  # 25: NBBBCNCCNBBNBNBBCHBHHBCHB
+  # 49: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
   let(:full_input) do
     <<~TXT
-      a
-      b
-      c
-      d
-      e
-      f
+      BNBBNCFHHKOSCHBKKSHN
+      
+      CH -> S
+      KK -> V
+      FS -> V
+      CN -> P
+      VC -> N
+      CB -> V
+      VK -> H
+      CF -> N
+      PO -> O
+      KC -> S
+      HC -> P
+      PP -> B
+      KO -> B
+      BK -> P
+      BH -> N
+      CC -> N
+      PC -> O
+      FK -> N
+      KF -> F
+      FH -> S
+      SS -> V
+      ON -> K
+      OV -> K
+      NK -> H
+      BO -> C
+      VP -> O
+      CS -> V
+      KS -> K
+      SK -> B
+      OP -> S
+      PK -> S
+      HF -> P
+      SV -> P
+      SB -> C
+      BC -> C
+      FP -> H
+      FC -> P
+      PB -> N
+      NV -> F
+      VO -> F
+      VH -> P
+      BB -> N
+      SF -> F
+      NB -> K
+      KB -> S
+      VV -> S
+      NP -> N
+      SO -> O
+      PN -> B
+      BP -> H
+      BV -> V
+      OB -> C
+      HV -> N
+      PF -> B
+      SP -> N
+      HN -> N
+      CV -> H
+      BN -> V
+      PS -> V
+      CO -> S
+      BS -> N
+      VB -> H
+      PV -> P
+      NN -> P
+      HS -> C
+      OS -> P
+      FB -> S
+      HO -> C
+      KH -> H
+      HB -> K
+      VF -> S
+      CK -> K
+      FF -> H
+      FN -> P
+      OK -> F
+      SC -> B
+      HH -> N
+      OH -> O
+      VS -> N
+      FO -> N
+      OC -> H
+      NF -> F
+      PH -> S
+      HK -> K
+      NH -> H
+      FV -> S
+      OF -> V
+      NC -> O
+      HP -> O
+      KP -> B
+      BF -> N
+      NO -> S
+      CP -> C
+      NS -> N
+      VN -> K
+      KV -> N
+      OO -> V
+      SN -> O
+      KN -> C
+      SH -> F
     TXT
   end
 
   describe "#part_one" do
     context "with simple input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(simple_input)).part_one).to eq(1)
+        expect(described_class.for(input: StringIO.new(simple_input)).part_one).to eq(1588)
       end
     end
 
     context "with full input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(full_input)).part_one).to eq(42)
+        expect(described_class.for(input: StringIO.new(full_input)).part_one).to eq(3009)
       end
     end
   end
 
   describe "#part_two" do
     context "with simple input" do
-      it "works" do
-        expect(described_class.for(input: StringIO.new(simple_input)).part_two).to eq(1)
+      fit "works" do
+        expect(described_class.for(input: StringIO.new(simple_input)).part_two).to eq(2188189693529)
       end
     end
 
