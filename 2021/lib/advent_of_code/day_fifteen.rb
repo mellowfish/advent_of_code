ff52af5c7a65adc8a33335a2ab23cf124ce4a642
Christopher Coleman <christopher.coleman@daveramsey.com>
2021-12-15 12:38:21 -0600

    [2021] Day 15 (part 1)

--- 2021/lib/advent_of_code/day_fifteen.rb
+++ 2021/lib/advent_of_code/day_fifteen.rb
@@ -1,19 +1,153 @@
 module AdventOfCode
   class DayFifteen
     def self.for(input: $stdin)
-      new(input.readlines)
+      new(ChitonousCave.parse(input.readlines(chomp: true)))
     end
 
-    def initialize(lines)
-      @lines = lines
+    attr_reader :cave
+
+    def initialize(cave)
+      @cave = cave
     end
 
     def part_one
-      raise "Not yet!"
+      cave.safest_path
     end
 
     def part_two
-      raise "Not yet!"
+      # puts cave.expand.matrix.map(&:join)
+      cave.expand.safest_path
+    end
+
+    class ChitonousCave < Shared::Model
+      def self.parse(lines)
+        new(matrix: lines.map { |line| line.chars.map(&:to_i) })
+      end
+
+      attribute :matrix, type: Array
+
+      def safest_path
+        distance_to_nodes[node_count - 1]
+      end
+
+      def expand(multiplier = 5)
+        new_matrix = Array.new(width * multiplier) { Array.new(width * multiplier, nil) }
+        multiplier.times do |y|
+          multiplier.times do |x|
+            delta = x + y
+            matrix.each_with_index do |cells, row|
+              cells.each_with_index do |cell, column|
+                new_cell = cell + delta
+                new_cell -= 9 if new_cell > 9
+                new_matrix[width * y + row][width * x + column] = new_cell
+              end
+            end
+          end
+        end
+        with(matrix: new_matrix)
+      end
+
+    private
+
+      def node_count
+        width * height
+      end
+
+      def find_shortest_path(destination, path = [])
+        if shortest_previous_neighbor[destination] == -1
+          find_shortest_path(shortest_previous_neighbor[destination], path)
+        end
+        path << destination
+      end
+
+      def distance_to_nodes
+        return @distance_to_nodes if defined?(@distance_to_nodes)
+
+        compute_paths
+
+        @distance_to_nodes
+      end
+
+      def shortest_previous_neighbor
+        return @shortest_previous_neighbor if defined?(@shortest_previous_neighbor)
+
+        compute_paths
+
+        @shortest_previous_neighbor
+      end
+
+      def compute_paths(source = 0)
+        infinity = node_count * 10
+        @distance_to_nodes = Array.new(node_count, infinity)
+        @shortest_previous_neighbor = Array.new(node_count, -1)
+        @distance_to_nodes[source] = 0
+        unvisited_nodes = (0...node_count).to_a
+
+        puts unvisited_nodes.size
+        until unvisited_nodes.empty?
+          puts unvisited_nodes.size if (unvisited_nodes.size % 1_000).zero?
+          node_to_visit = nil
+
+          unvisited_nodes.find do |minimum_node_index|
+            if node_to_visit.nil? || (@distance_to_nodes[minimum_node_index] < @distance_to_nodes[node_to_visit])
+              node_to_visit = minimum_node_index
+            end
+          end
+
+          if @distance_to_nodes[node_to_visit] == infinity
+            binding.pry
+            break
+          end
+
+          unvisited_nodes.delete(node_to_visit)
+
+          # puts "checking node #{node_to_visit} (#{node_to_visit / width},#{node_to_visit % width})"
+
+          node_matrix[node_to_visit].each_with_index do |known_distance, target_index|
+            next if known_distance.zero?
+
+            new_distance = distance_to_nodes[node_to_visit] + known_distance
+
+            if new_distance < distance_to_nodes[target_index]
+              @distance_to_nodes[target_index] = new_distance
+              @shortest_previous_neighbor[target_index] = node_to_visit
+            end
+          end
+        end
+      end
+
+      def node_matrix
+        @node_matrix ||=
+          Array.new(node_count) { Array.new(node_count, 0) }.tap do |nodes|
+            matrix.each_with_index do |cells, row|
+              cells.each_with_index do |level, column|
+                node_index = row * width + column
+                neighbors_of(row, column).each do |neighbor_row, neighbor_column|
+                  neighbor_node_index = neighbor_row * width + neighbor_column
+                  nodes[neighbor_node_index][node_index] = level
+                end
+              end
+            end
+          end
+      end
+
+      def neighbors_of(row, column)
+        [].tap do |neighbors|
+          neighbors << [row - 1, column] if row.positive?
+          neighbors << [row + 1, column] if row < (height - 1)
+
+          neighbors << [row, column - 1] if column.positive?
+          neighbors << [row, column + 1] if column < (width - 1)
+        end
+      end
+
+      def height
+        matrix.size
+      end
+
+      def width
+        matrix.first.size
+      end
     end
   end
 end
