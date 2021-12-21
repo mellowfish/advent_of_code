926bdb401e99d102c6b1807d973664d22f01c401
Christopher Coleman <christopher.coleman@daveramsey.com>
2021-12-21 12:44:39 -0600

    [2021] Day 21 - part 1

--- 2021/lib/advent_of_code/day_twenty_one.rb
+++ 2021/lib/advent_of_code/day_twenty_one.rb
@@ -1,19 +1,113 @@
 module AdventOfCode
   class DayTwentyOne
     def self.for(input: $stdin)
-      new(input.readlines)
+      new(Game.parse(input.readlines(chomp: true)))
     end
 
-    def initialize(lines)
-      @lines = lines
+    attr_reader :game
+
+    def initialize(game)
+      @game = game
     end
 
     def part_one
-      raise "Not yet!"
+      @game = game.take_turn until game.won?
+      game.score
     end
 
     def part_two
       raise "Not yet!"
+    end
+
+    class Player < Shared::Model
+      attribute :position, type: Integer
+      attribute :points, type: Integer, default: 0
+
+      def move(distance)
+        new_position = ((position + distance - 1) % 10) + 1
+
+        with(
+          position: new_position,
+          points: points + new_position
+        )
+      end
+
+      def won?
+        points >= 1000
+      end
+    end
+
+    class DeterministicDie
+      def initialize
+        @value = 0
+      end
+
+      def roll
+        @value += 1
+        @value = 1 if @value == 101
+        @value
+      end
+    end
+
+    class Game < Shared::Model
+      ROLLS_PER_TURN = 3
+
+      def self.parse(lines)
+        one, two = lines.map { |line| line[-1].to_i }
+        new(
+          players: [Player.new(position: one), Player.new(position: two)],
+          die: DeterministicDie.new
+        )
+      end
+
+      attribute :players, type: Array
+      attribute :die, respond_to: :roll
+      attribute :rolls, type: Integer, default: 0
+
+      def take_turn
+        with(
+          players: other_players + [current_player_next],
+          rolls: rolls + die_values.size
+        )
+      end
+
+      def won?
+        players.any?(&:won?)
+      end
+
+      def losers
+        players.reject(&:won?)
+      end
+
+      def score
+        losers.sum(&:points) * rolls
+      end
+
+      def print
+        puts "Player #{current_player_number} rolls #{die_values.join('+')} and moves to space #{current_player_next.position} for a total score of #{current_player_next.points}."
+      end
+
+    private
+
+      def current_player_next
+        @current_player_next ||= current_player.move(die_values.sum)
+      end
+
+      def die_values
+        @die_values ||= Array.new(ROLLS_PER_TURN) { die.roll }
+      end
+
+      def current_player_number
+        (rolls / 3) % players.size + 1
+      end
+
+      def current_player
+        players.first
+      end
+
+      def other_players
+        players[1..]
+      end
     end
   end
 end
--- 2021/spec/lib/advent_of_code/day_twenty_one_spec.rb
+++ 2021/spec/lib/advent_of_code/day_twenty_one_spec.rb
@@ -1,46 +1,41 @@
 RSpec.describe AdventOfCode::DayTwentyOne do
   let(:simple_input) do
     <<~TXT
-      a
-      b
-      c
+      Player 1 starting position: 4
+      Player 2 starting position: 8
     TXT
   end
 
   let(:full_input) do
     <<~TXT
-      a
-      b
-      c
-      d
-      e
-      f
+      Player 1 starting position: 3
+      Player 2 starting position: 5
     TXT
   end
 
   describe "#part_one" do
     context "with simple input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(simple_input)).part_one).to eq(1)
+        expect(described_class.for(input: StringIO.new(simple_input)).part_one).to eq(739785)
       end
     end
 
     context "with full input" do
       it "works" do
-        expect(described_class.for(input: StringIO.new(full_input)).part_one).to eq(42)
+        expect(described_class.for(input: StringIO.new(full_input)).part_one).to eq(720750)
       end
     end
   end
 
   describe "#part_two" do
     context "with simple input" do
-      it "works" do
-        expect(described_class.for(input: StringIO.new(simple_input)).part_two).to eq(1)
+      xit "works" do
+        expect(described_class.for(input: StringIO.new(simple_input)).part_two).to eq(444356092776315)
       end
     end
 
     context "with full input" do
-      it "works" do
+      xit "works" do
         expect(described_class.for(input: StringIO.new(full_input)).part_two).to eq(42)
       end
     end
