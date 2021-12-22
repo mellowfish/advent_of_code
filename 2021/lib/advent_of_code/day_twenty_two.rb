d1ba1a4113c4560f7c331d14524aa1a66bfb2294
Christopher Coleman <christopher.coleman@daveramsey.com>
2021-12-22 10:25:33 -0600

    [2021] Day 22 - part 1

--- 2021/lib/advent_of_code/day_twenty_two.rb
+++ 2021/lib/advent_of_code/day_twenty_two.rb
@@ -1,19 +1,106 @@
 module AdventOfCode
   class DayTwentyTwo
     def self.for(input: $stdin)
-      new(input.readlines)
+      new(ReactorCore.parse_instructions(input.readlines(chomp: true)))
     end
 
-    def initialize(lines)
-      @lines = lines
+    attr_reader :core
+
+    def initialize(core)
+      @core = core
     end
 
     def part_one
-      raise "Not yet!"
+      core.boot.active_cube_count
     end
 
     def part_two
       raise "Not yet!"
+    end
+
+    class ReactorCore < Shared::Model
+      class Instruction < Shared::Model
+        def self.parse(str)
+          action_str, ranges_str = str.split(" ")
+          action = action_str == "on"
+          x_range, y_range, z_range =
+            ranges_str.split(",").map do |range_str|
+              min, max = range_str[2..].split("..").map(&:to_i)
+              min..max
+            end
+
+          new(action: action, x_range: x_range, y_range: y_range, z_range: z_range)
+        end
+
+        attribute :action, type: :boolean
+
+        attribute :x_range, type: Range
+        attribute :y_range, type: Range
+        attribute :z_range, type: Range
+
+        def in_scope?
+          return @in_scope if defined?(@in_scope)
+
+          @in_scope = valid_range?(x_range) && valid_range?(y_range) && valid_range?(z_range)
+        end
+
+        def cover?(x:, y:, z:) # rubocop:disable Naming/MethodParameterName
+          x_range.cover?(x) && y_range.cover?(y) && z_range.cover?(z)
+        end
+
+        def evaluate_for_cube(current_value:, x:, y:, z:) # rubocop:disable Naming/MethodParameterName
+          return current_value unless cover?(x: x, y: y, z: z)
+
+          action
+        end
+
+      private
+
+        def valid_range?(range)
+          range.end < 51 && range.begin > -51
+        end
+      end
+
+      def self.parse_instructions(lines)
+        new(startup_instructions: lines.map { |line| Instruction.parse(line) })
+      end
+
+      attribute :startup_instructions, type: Array, default: []
+      attribute :active_cube_count, type: Integer, default: 0
+
+      def boot
+        new_active_cube_count = 0
+        (-50..50).each do |x|
+          (-50..50).each do |y|
+            (-50..50).each do |z|
+              valid_instructions.reduce(false) do |current_value, instruction|
+                instruction.evaluate_for_cube(current_value: current_value, x: x, y: y, z: z)
+              end.tap { |active| new_active_cube_count += 1 if active }
+            end
+          end
+        end
+
+        with(active_cube_count: new_active_cube_count)
+      end
+
+      def reboot
+        new_active_cube_count = 0
+        (-50..50).each do |x|
+          (-50..50).each do |y|
+            (-50..50).each do |z|
+              startup_instructions.reduce(false) do |current_value, instruction|
+                instruction.evaluate_for_cube(current_value: current_value, x: x, y: y, z: z)
+              end.tap { |active| new_active_cube_count += 1 if active }
+            end
+          end
+        end
+
+        with(active_cube_count: new_active_cube_count)
+      end
+
+      def valid_instructions
+        @valid_instructions ||= startup_instructions.select(&:in_scope?)
+      end
     end
   end
 end
