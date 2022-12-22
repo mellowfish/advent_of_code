bf3daf821a6f462ebac29730099206dd3178f04d
Christopher Coleman <christopher.coleman@daveramsey.com>
2022-12-22 16:52:26 -0600

    [2022] Day 22 part 2 WIP

--- 2022/day_twenty_two/src/lib.rs
+++ 2022/day_twenty_two/src/lib.rs
@@ -1,10 +1,9 @@
-use std::borrow::Borrow;
+extern crate core;
+
 use std::collections::HashMap;
 use std::ops::RangeInclusive;
-use std::thread::sleep;
-use std::time::Duration;
 
-#[derive(Clone)]
+#[derive(Clone, Copy, Debug)]
 enum Direction {
     North,
     South,
@@ -57,6 +56,144 @@ impl MapTile {
     }
 }
 
+#[derive(Clone, Debug, PartialEq, Copy)]
+enum MapFace {
+    Top,    // 1
+    Back,   // 2
+    Left,   // 3
+    Front,  // 4
+    Bottom, // 5
+    Right,  // 6
+}
+
+impl MapFace {
+    fn from_grid(row: i32, column: i32) -> Option<MapFace> {
+        match (row, column) {
+            (0, 2) => Some(MapFace::Top),
+            (1, 0) => Some(MapFace::Back),
+            (1, 1) => Some(MapFace::Left),
+            (1, 2) => Some(MapFace::Front),
+            (2, 2) => Some(MapFace::Bottom),
+            (2, 3) => Some(MapFace::Right),
+            (0, 0) | (0, 1) | (0, 3) | (1, 3) | (2, 0) | (2, 1) => None, // blank spots on map (triggers portal)
+            (-1, _) | (_, -1) | (3, _) | (_, 4) => None, // just outside map (triggers portal)
+            _ => panic!("Invalid grid coordinates: {row}, {column}") // should never happen
+
+        }
+    }
+
+    fn origin(&self, grid_size: usize) -> Point {
+        let grid_size = grid_size as i32;
+        match self {
+            MapFace::Top => Point { x: 2 * grid_size, y: 0 },
+            MapFace::Back => Point { x: 0, y: grid_size },
+            MapFace::Left => Point { x: grid_size, y: grid_size },
+            MapFace::Front => Point { x: 2 * grid_size, y: grid_size },
+            MapFace::Bottom => Point { x: 2 * grid_size, y: 2 * grid_size },
+            MapFace::Right => Point { x: 3 * grid_size, y: 2 * grid_size },
+        }
+    }
+
+    fn portal(from: MapFace, pointed: Direction, grid_point: Point, grid_size: usize) -> (MapFace, Direction, Point) {
+        let new_face =
+            match (from.clone(), pointed.clone()) {
+                (MapFace::Back, Direction::North)
+                | (MapFace::Left, Direction::North)
+                | (MapFace::Right, Direction::East)
+                    => MapFace::Top,
+
+                (MapFace::Top, Direction::North)
+                | (MapFace::Bottom, Direction::South)
+                | (MapFace::Right, Direction::South)
+                    => MapFace::Back,
+
+                (MapFace::Top, Direction::West)
+                | (MapFace::Bottom, Direction::West)
+                    => MapFace::Left,
+
+                (MapFace::Top, Direction::East)
+                | (MapFace::Back, Direction::West)
+                | (MapFace::Front, Direction::East)
+                    => MapFace::Right,
+
+                (MapFace::Back, Direction::South)
+                | (MapFace::Left, Direction::South)
+                    => MapFace::Bottom,
+
+                (MapFace::Right, Direction::North)
+                    => MapFace::Front,
+
+                (MapFace::Top, Direction::South)
+                | (MapFace::Front, Direction::North) | (MapFace::Front, Direction::West) | (MapFace::Front, Direction::South)
+                | (MapFace::Back, Direction::East)
+                | (MapFace::Left, Direction::West) | (MapFace::Left, Direction::East)
+                | (MapFace::Bottom, Direction::North) | (MapFace::Bottom, Direction::East)
+                | (MapFace::Right, Direction::West)
+                    => panic!("Normal traverse, not a portal!"),
+            };
+
+        let (new_direction, new_grid_point) = Self::traverse(from, new_face.clone(), grid_point, grid_size);
+
+        (new_face, new_direction, new_grid_point)
+    }
+
+    fn traverse(from: MapFace, to: MapFace, from_grid_point: Point, grid_size: usize) -> (Direction, Point) {
+        match (from.clone(), to.clone()) {
+            // connected faces (opposite edges)
+            (MapFace::Top, MapFace::Front)
+            | (MapFace::Front, MapFace::Bottom) => (Direction::South, from_grid_point.wrap_to_grid_top()),
+
+            (MapFace::Bottom, MapFace::Front)
+            | (MapFace::Front, MapFace::Top) => (Direction::North, from_grid_point.wrap_to_grid_bottom(grid_size)),
+
+            (MapFace::Back, MapFace::Left)
+            | (MapFace::Left, MapFace::Front)
+            | (MapFace::Bottom, MapFace::Right) => (Direction::East, from_grid_point.wrap_to_grid_left()),
+
+            (MapFace::Front, MapFace::Left)
+            | (MapFace::Left, MapFace::Back)
+            | (MapFace::Right, MapFace::Bottom) => (Direction::West, from_grid_point.wrap_to_grid_right(grid_size)),
+
+            // transposed faces (edges rotated 90 degrees)
+
+            (MapFace::Top, MapFace::Left) => (Direction::South, from_grid_point.transpose_grid_top_right_edge()),
+            (MapFace::Left, MapFace::Top) => (Direction::East, from_grid_point.transpose_grid_top_right_edge()),
+
+
+            (MapFace::Front, MapFace::Right) => (Direction::South, from_grid_point.transpose_grid_top_left_edge(grid_size)),
+            (MapFace::Right, MapFace::Front) => (Direction::West, from_grid_point.transpose_grid_top_left_edge(grid_size)),
+
+            (MapFace::Left, MapFace::Bottom)
+            | (MapFace::Right, MapFace::Back) => (Direction::East, from_grid_point.transpose_grid_top_left_edge(grid_size)),
+
+            (MapFace::Bottom, MapFace::Left)
+            | (MapFace::Back, MapFace::Right) => (Direction::North, from_grid_point.transpose_grid_top_left_edge(grid_size)),
+
+            // reversed faces (same edge joined backward)
+
+            (MapFace::Top, MapFace::Back)
+            | (MapFace::Back, MapFace::Top) => (Direction::South, from_grid_point.invert_x(grid_size)),
+
+            (MapFace::Back, MapFace::Bottom)
+            | (MapFace::Bottom, MapFace::Back) => (Direction::North, from_grid_point.invert_x(grid_size)),
+
+            (MapFace::Top, MapFace::Right)
+            | (MapFace::Right, MapFace::Top) => (Direction::West, from_grid_point.invert_y(grid_size)),
+
+
+            (MapFace::Top, MapFace::Bottom) | (MapFace::Bottom, MapFace::Top)
+            | (MapFace::Left, MapFace::Right) | (MapFace::Right, MapFace::Left)
+            | (MapFace::Back, MapFace::Front) | (MapFace::Front, MapFace::Back)
+                => panic!("Bending spacetime! Trying to go from {:?} to {:?}", from, to),
+
+            (MapFace::Top, MapFace::Top) | (MapFace::Bottom, MapFace::Bottom)
+            | (MapFace::Left, MapFace::Left) | (MapFace::Right, MapFace::Right)
+            | (MapFace::Back, MapFace::Back) | (MapFace::Front, MapFace::Front)
+                => panic!("From and to must be different")
+        }
+    }
+}
+
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 struct Point {
     x:  i32,
@@ -78,6 +215,42 @@ impl Point {
 
     fn right(&self) -> Self {
         Self { x: self.x + 1, y: self.y }
+    }
+
+    fn plus(&self, delta: &Self) -> Self {
+        Self { x: self.x + delta.x, y: self.y + delta.y }
+    }
+
+    fn wrap_to_grid_top(&self) -> Self {
+        Self { x: self.x, y: 0 }
+    }
+
+    fn wrap_to_grid_bottom(&self, grid_size: usize) -> Self {
+        Self { x: self.x, y: (grid_size - 1) as i32 }
+    }
+
+    fn wrap_to_grid_left(&self) -> Self {
+        Self { x: 0, y: self.y }
+    }
+
+    fn wrap_to_grid_right(&self, grid_size: usize) -> Self {
+        Self { x: (grid_size - 1) as i32, y: self.y }
+    }
+
+    fn transpose_grid_top_right_edge(&self) -> Self {
+        Self { x: self.y, y: self.x }
+    }
+
+    fn transpose_grid_top_left_edge(&self, grid_size: usize) -> Self {
+        Self { x: grid_size as i32 - (self.y + 1), y: grid_size as i32 - (self.x + 1) }
+    }
+
+    fn invert_y(&self, grid_size: usize) -> Self {
+        Self { x: self.x, y: grid_size as i32 - (self.y + 1) }
+    }
+
+    fn invert_x(&self, grid_size: usize) -> Self {
+        Self { x: grid_size as i32 - (self.x + 1), y: self.y }
     }
 
     fn move_one_step(&self, direction: &Direction) -> Self {
@@ -205,7 +378,7 @@ impl PasswordMap {
         self.row_ranges.iter().map(|row_range| *row_range.end()).max().unwrap() as usize
     }
 
-    fn decode_password(&self) -> usize {
+    fn decode_password(&self, folded: bool) -> usize {
         let mut current_point = Point { x: self.start_of_row(0), y: 0 };
         let mut current_direction = Direction::East;
         let mut path: HashMap<Point, Direction> = HashMap::new();
@@ -216,7 +389,15 @@ impl PasswordMap {
 
             match instruction {
                 Instruction::Move(distance) => {
-                    current_point = self.walk_forward(&current_point, &current_direction, *distance, &mut path);
+                    if folded {
+                        let (new_current_point, new_current_direction) = self.walk_forward_folded(current_point, current_direction.clone(), *distance, &mut path);
+                        current_point = new_current_point;
+                        current_direction = new_current_direction;
+                    } else {
+                        let (new_current_point, new_current_direction) = self.walk_forward(current_point, current_direction.clone(), *distance, &mut path);
+                        current_point = new_current_point;
+                        current_direction = new_current_direction;
+                    }
                 },
                 Instruction::TurnLeft => {
                     current_direction = current_direction.turn_left();
@@ -231,14 +412,14 @@ impl PasswordMap {
         current_point.score() + current_direction.score()
     }
 
-    fn walk_forward(&self, current_point: &Point, current_direction: &Direction, distance: usize, path: &mut HashMap<Point, Direction>) -> Point {
+    fn walk_forward(&self, current_point: Point, current_direction: Direction, distance: usize, path: &mut HashMap<Point, Direction>) -> (Point, Direction) {
         let mut next_point = current_point.clone();
 
         // println!("Walking forward");
         // self.print(&path, &next_point, &current_direction);
 
         for _ in 0..distance {
-            let mut possible_next_point = next_point.move_one_step(current_direction);
+            let mut possible_next_point = next_point.move_one_step(&current_direction);
             // dbg!(&possible_next_point);
 
             match self.map.get(&possible_next_point) {
@@ -250,7 +431,7 @@ impl PasswordMap {
                 Some(MapTile::Wall) => {
                     // self.print(&path, &next_point, &current_direction);
                     // println!("Hit a wall");
-                    return next_point;
+                    return (next_point, current_direction.clone());
                 },
                 None => {
                     // println!("Wrapping around");
@@ -274,7 +455,7 @@ impl PasswordMap {
                             Some(MapTile::Wall) => {
                                 // self.print(&path, &next_point, &current_direction);
                                 // println!("Hit a wall");
-                                return next_point;
+                                return (next_point, current_direction.clone());
                             },
                             None => {
                                 possible_next_point = possible_next_point.move_one_step(&current_direction);
@@ -285,10 +466,101 @@ impl PasswordMap {
             }
         }
 
-        next_point
+        (next_point, current_direction)
     }
 
-    // cargo test --package day_twenty_two --lib tests::part_one_example -- --exact --nocapture
+    fn walk_forward_folded(&self, current_point: Point, current_direction: Direction, distance: usize, path: &mut HashMap<Point, Direction>) -> (Point, Direction) {
+        let mut next_point = current_point.clone();
+        let (mut next_face, mut next_grid_point)  = self.face_for(&next_point).unwrap();
+        let mut next_direction = current_direction.clone();
+        // dbg!(next_point, next_direction, next_face);
+
+        for _ in 0..distance {
+            let mut possible_next_point = next_point.move_one_step(&next_direction);
+            let mut possible_next_direction = next_direction.clone();
+            let mut possible_next_face = next_face.clone();
+            let mut possible_next_grid_point = next_grid_point.clone();
+
+            // dbg!(possible_next_point, possible_next_direction, possible_next_face);
+
+            match self.face_for(&possible_next_point) {
+                Some((_possible_next_face, _possible_next_grid_point)) => {
+                    possible_next_face = _possible_next_face;
+                    possible_next_grid_point = _possible_next_grid_point;
+
+                    // dbg!(next_face, possible_next_face);
+                    if next_face != possible_next_face {
+                        let (_possible_next_direction, _possible_next_grid_point) =
+                            MapFace::traverse(next_face, possible_next_face, possible_next_grid_point, self.grid_size());
+
+                        possible_next_direction = _possible_next_direction;
+                        possible_next_grid_point = _possible_next_grid_point;
+                    }
+                },
+                None => {
+                    // println!("Time to portal!");
+                    // dbg!(next_face, next_direction, next_grid_point);
+                    let (_possible_next_face, _possible_next_direction, _possible_next_grid_point) =
+                        MapFace::portal(next_face, next_direction, next_grid_point, self.grid_size());
+                    possible_next_face = _possible_next_face;
+                    possible_next_direction = _possible_next_direction;
+                    possible_next_grid_point = _possible_next_grid_point;
+                    // dbg!(possible_next_grid_point, possible_next_direction, possible_next_face);
+                    possible_next_point = self.point_from_grid_point(&possible_next_grid_point, &possible_next_face);
+                    // dbg!(possible_next_point);
+                }
+            }
+
+            // having determined my possible next step, actually try to move in that direction
+            match self.map.get(&possible_next_point) {
+                Some(MapTile::Space) => {
+                    path.insert(next_point.clone(), next_direction.clone());
+                    next_point = possible_next_point;
+                    next_direction = possible_next_direction;
+                    next_face = possible_next_face;
+                    next_grid_point = possible_next_grid_point;
+                    // self.print(path, &next_point, &next_direction);
+                },
+                Some(MapTile::Wall) => {
+                    // self.print(path, &next_point, &next_direction);
+                    return (next_point, next_direction)
+                },
+                None => {
+                    dbg!(possible_next_point, possible_next_direction, possible_next_face);
+                    panic!("Somehow I fell off the cube...");
+                }
+            }
+        }
+
+        (next_point, next_direction)
+    }
+
+    fn grid_size(&self) -> usize {
+        self.height() / 3
+    }
+
+    fn face_for(&self, point: &Point) -> Option<(MapFace, Point)> {
+        let grid_size = self.grid_size() as i32;
+        let grid_row = point.y / grid_size;
+        let grid_column = point.x / grid_size;
+        dbg!(grid_size, grid_row, grid_column);
+
+        match MapFace::from_grid(grid_row, grid_column) {
+            Some(face) => {
+                let interior_grid_row = point.y % grid_size;
+                let interior_grid_column = point.x % grid_size;
+
+                Some((face, Point { x: interior_grid_column, y: interior_grid_row }))
+            },
+            None => None
+        }
+    }
+
+    fn point_from_grid_point(&self, grid_point: &Point, face: &MapFace) -> Point {
+        grid_point.plus(&face.origin(self.grid_size()))
+    }
+
+    // cargo test --package day_twenty_two --lib tests::part_two_example -- --exact --nocapture
     #[allow(dead_code)]
     fn print(&self, path: &HashMap<Point, Direction>, current_point: &Point, current_direction: &Direction) {
         for (row_index, row_range) in self.row_ranges.iter().enumerate() {
@@ -335,11 +607,22 @@ mod tests {
 
     #[test]
     fn part_one_example() {
-        assert_eq!(PasswordMap::new(fs::read_to_string("example_input.txt").unwrap().as_str()).decode_password(), 6032);
+        assert_eq!(PasswordMap::new(fs::read_to_string("example_input.txt").unwrap().as_str()).decode_password(false), 6032);
     }
 
     #[test]
     fn part_one() {
-        assert_eq!(PasswordMap::new(fs::read_to_string("input.txt").unwrap().as_str()).decode_password(), 30552);
+        assert_eq!(PasswordMap::new(fs::read_to_string("input.txt").unwrap().as_str()).decode_password(false), 30552);
+    }
+
+    #[test]
+    fn part_two_example() {
+        assert_eq!(PasswordMap::new(fs::read_to_string("example_input.txt").unwrap().as_str()).decode_password(true), 5031);
+    }
+
+    // TODO: the shape of the input cube/map is different than the example... can I somehow code a dynamic shape (with fixed grid size) that I pass in?
+    #[test]
+    fn part_two() {
+        assert_eq!(PasswordMap::new(fs::read_to_string("input.txt").unwrap().as_str()).decode_password(true), 5031);
     }
 }
