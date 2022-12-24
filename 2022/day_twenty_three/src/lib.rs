56d35c8567f2dac803b5f5a24ff3c3788b57af31
Christopher Coleman <mellowfish@fastmail.fm>
2022-12-24 13:20:53 -0600

    [2022] Day 23

--- 2022/day_twenty_three/src/lib.rs
+++ 2022/day_twenty_three/src/lib.rs
@@ -25,6 +25,34 @@ impl Point {
     fn right(&self) -> Self {
         Self { x: self.x + 1, y: self.y }
     }
+
+    fn neighbors(&self) -> Vec<Point> {
+        let mut neighbors = vec![];
+        let mut current = self.up(); // north
+        let mut next = current.left(); // northwest
+        neighbors.push(current);
+        current = next;
+        next = current.down(); // west
+        neighbors.push(current);
+        current = next;
+        next = current.down(); // southwest
+        neighbors.push(current);
+        current = next;
+        next = current.right(); // south
+        neighbors.push(current);
+        current = next;
+        next = current.right(); // southeast
+        neighbors.push(current);
+        current = next;
+        next = current.up(); // east
+        neighbors.push(current);
+        current = next;
+        next = current.up(); // northeast
+        neighbors.push(current);
+        neighbors.push(next);
+
+        neighbors
+    }
 }
 
 #[derive(Clone)]
@@ -52,26 +80,46 @@ impl PlantingParty {
         Self { elves }
     }
 
-    fn run_rounds(&mut self, rounds: usize) {
+    fn run_rounds(&mut self, rounds: usize) -> usize {
         let original_directions = vec!['N', 'S', 'W', 'E'];
         let mut directions : Cycle<Iter<char>> = original_directions.iter().cycle();
+
+        println!("Start");
         self.print();
-        for _ in 0..rounds {
-            self.run_round(&mut directions);
+
+        for n in 0..rounds {
+            let round = n + 1;
+            let moves = self.run_round(&mut directions);
+
+            if moves == 0 {
+                println!("Reached steady state after {} round(s):", round);
+                self.print();
+                return round
+            }
+
+            if round % 100 == 0 {
+                println!("Round: {}: {} move(s)", round, moves);
+                self.print();
+            }
             directions.next();
-            self.print();
         }
+
+        rounds
     }
 
     fn has_elf_at(&self, point: &Point) -> bool {
         self.elves.contains_key(point)
     }
 
-    fn run_round(&mut self, directions: &mut Cycle<Iter<char>>) {
+    fn run_round(&mut self, directions: &mut Cycle<Iter<char>>) -> usize {
         let mut proposed_moves : HashMap<Point, Vec<Elf>> = HashMap::new();
 
         for (point, elf) in self.elves.iter() {
-            for direction in directions.take(4) {
+            if point.neighbors().iter().all(|neighbor| { !self.has_elf_at(neighbor)}) {
+                continue;
+            };
+
+            for direction in directions.clone().take(4) {
                 match direction {
                     'N' => {
                         if let Some(point) = self.consider_moving_north(elf) {
@@ -103,14 +151,19 @@ impl PlantingParty {
             }
         }
 
+        let mut moves = 0;
         for (point, elves) in proposed_moves.iter() {
             if elves.len() > 1 {
                 continue;
             }
 
+            moves += 1;
+
             self.elves.remove(&elves[0].current_position);
             self.elves.insert(point.clone(), Elf { current_position: point.clone()});
         }
+
+        moves
     }
 
     fn record_proposed_move(&self, proposed_moves: &mut HashMap<Point, Vec<Elf>>, elf: &Elf, point: Point) {
@@ -177,10 +230,10 @@ impl PlantingParty {
     }
 
     fn dimensions(&self) -> (Point, Point) {
-        let mut min_x : i32 = 0;
-        let mut min_y : i32 = 0;
-        let mut max_x : i32 = 0;
-        let mut max_y : i32 = 0;
+        let mut min_x : i32 = i32::MAX;
+        let mut min_y : i32 = i32::MAX;
+        let mut max_x : i32 = i32::MIN;
+        let mut max_y : i32 = i32::MIN;
 
         for (point, _) in self.elves.iter() {
             min_y = min(min_y, point.y);
@@ -202,7 +255,6 @@ impl PlantingParty {
                     land += 1
                 }
             }
-            println!()
         }
 
         land
@@ -220,6 +272,40 @@ mod tests {
 
         planting_party.run_rounds(10);
 
-        assert_eq!(planting_party.open_land(), 24);
+        assert_eq!(planting_party.open_land(), 25);
+    }
+
+    #[test]
+    fn part_one_example() {
+        let mut planting_party = PlantingParty::new(fs::read_to_string("example_input.txt").unwrap().as_str());
+
+        planting_party.run_rounds(10);
+
+        assert_eq!(planting_party.open_land(), 110);
+    }
+
+    #[test]
+    fn part_one() {
+        let mut planting_party = PlantingParty::new(fs::read_to_string("input.txt").unwrap().as_str());
+
+        planting_party.run_rounds(10);
+
+        assert_eq!(planting_party.open_land(), 4249);
+    }
+
+    #[test]
+    fn part_two_example() {
+        assert_eq!(
+            PlantingParty::new(fs::read_to_string("example_input.txt").unwrap().as_str()).run_rounds(100),
+            20
+        );
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(
+            PlantingParty::new(fs::read_to_string("input.txt").unwrap().as_str()).run_rounds(1_000),
+            980
+        );
     }
 }
