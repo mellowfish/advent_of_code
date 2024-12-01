44c77796048e9ed8e00812bc3315cb1e55e543c6
Christopher Coleman <mellowfish@fastmail.fm>
2024-12-01 07:55:02 -0600

    [2024] Day 1

--- 2024/src/main/java/org/mellowfish/adventofcode/mmxxiv/DayOne.java
+++ 2024/src/main/java/org/mellowfish/adventofcode/mmxxiv/DayOne.java
@@ -1,20 +1,64 @@
 package org.mellowfish.adventofcode.mmxxiv;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class DayOne {
     private final String input;
+    private final List<Integer> listOne;
+    private final List<Integer> listTwo;
 
     public static DayOne with(String input) {
         return new DayOne(input);
     }
 
+    DayOne(String input) {
+        this.input = input;
+        this.listOne = new ArrayList<>();
+        this.listTwo = new ArrayList<>();
+        extractLists();
+    }
+
     public int partOne() {
-        return 41;
+        var iteratorOne = listOne.iterator();
+        var iteratorTwo = listTwo.iterator();
+        int difference = 0;
+        int valueOne;
+        int valueTwo;
+        while (iteratorOne.hasNext() && iteratorTwo.hasNext()) {
+            valueOne = iteratorOne.next();
+            valueTwo = iteratorTwo.next();
+            difference += Math.abs(valueOne - valueTwo);
+        }
+
+        return difference;
     }
 
     public int partTwo() {
-        return 43;
+        return listOne.stream()
+                .reduce(
+                        0,
+                        (similarity, valueOne) -> similarity
+                                + listTwo.stream().reduce(0, (acc, valueTwo) -> {
+                                    if (valueOne.equals(valueTwo)) {
+                                        return acc + valueOne;
+                                    }
+                                    return acc;
+                                }));
+    }
+
+    private void extractLists() {
+        Scanner scanner = new Scanner(input);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split("\s+");
+            listOne.add(Integer.parseInt(parts[0]));
+            listTwo.add(Integer.parseInt(parts[1]));
+        }
+        listOne.sort(Integer::compareTo);
+        listTwo.sort(Integer::compareTo);
     }
 }
--- 2024/src/test/java/org/mellowfish/adventofcode/mmxxiv/DayOneTest.java
+++ 2024/src/test/java/org/mellowfish/adventofcode/mmxxiv/DayOneTest.java
@@ -2,21 +2,42 @@ package org.mellowfish.adventofcode.mmxxiv;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import org.junit.jupiter.api.Test;
 
 class DayOneTest {
-    String fullInput() throws IOException {
-        return Files.readString(Paths.get("../resources/day_one_full_input.txt"));
+    static final String SAMPLE_INPUT =
+            """
+                3   4
+                4   3
+                2   5
+                1   3
+                3   9
+                3   3
+                """;
+
+    String fullInput() throws Exception {
+        return Files.readString(Paths.get("src/test/resources/day_one_full_input.txt"));
     }
 
-    void testPartOne() {
-        assertEquals(42, DayOne.with("""
-                """).partOne());
+    @Test
+    void testPartOneExample() {
+        assertEquals(11, DayOne.with(SAMPLE_INPUT).partOne());
     }
 
-    void testPartTwo() throws Exception {
-        assertEquals(42, DayOne.with(fullInput()).partTwo());
+    @Test
+    void testPartOneFullInput() throws Exception {
+        assertEquals(2066446, DayOne.with(fullInput()).partOne());
+    }
+
+    @Test
+    void testPartTwoExample() {
+        assertEquals(31, DayOne.with(SAMPLE_INPUT).partTwo());
+    }
+
+    @Test
+    void testPartTwoWithFullInput() throws Exception {
+        assertEquals(24931009, DayOne.with(fullInput()).partTwo());
     }
 }
