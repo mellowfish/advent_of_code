677d1eb493912775a26c7ce1fa801e02a56fdba3
Christopher Coleman <mellowfish@fastmail.fm>
2024-12-07 09:36:15 -0600

    [2024] Day 7

--- 2024/src/main/java/org/mellowfish/adventofcode/mmxxiv/DaySeven.java
+++ 2024/src/main/java/org/mellowfish/adventofcode/mmxxiv/DaySeven.java
@@ -1,7 +1,61 @@
 package org.mellowfish.adventofcode.mmxxiv;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+enum Operator {
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+    CONCATENATE;
+
+    public BigInteger apply(BigInteger a, BigInteger b) {
+        return switch (this) {
+            case ADD -> a.add(b);
+            case SUBTRACT -> a.subtract(b);
+            case MULTIPLY -> a.multiply(b);
+            case DIVIDE -> a.divide(b);
+            case CONCATENATE -> new BigInteger(a.toString() + b.toString());
+        };
+    }
+}
+
+record PartialEquation(BigInteger expectedResult, List<BigInteger> operands, List<Operator> operators) {
+    public boolean evalute() {
+        if (!complete()) {
+            throw new RuntimeException("Evaluate called on incomplete equation");
+        }
+
+        BigInteger calculatedResult = operands.getFirst();
+        for (int i = 0; i < operators.size(); i++) {
+            calculatedResult = operators.get(i).apply(calculatedResult, operands.get(i + 1));
+        }
+        return calculatedResult.equals(expectedResult);
+    }
+
+    public boolean complete() {
+        return operands.size() - operators.size() == 1;
+    }
+
+    public boolean canBeSolvedWith(List<Operator> possibleOperators) {
+        if (complete()) {
+            return evalute();
+        }
+
+        return possibleOperators.stream().anyMatch(operator -> {
+            List<Operator> newOperators = new ArrayList<>(operators);
+            newOperators.add(operator);
+            return new PartialEquation(expectedResult, operands, newOperators).canBeSolvedWith(possibleOperators);
+        });
+    }
+}
+
 public class DaySeven {
     private final String input;
+    private final ArrayList<PartialEquation> equations;
 
     public static DaySeven with(String input) {
         return new DaySeven(input);
@@ -9,13 +63,33 @@ public class DaySeven {
 
     DaySeven(String input) {
         this.input = input;
+        this.equations = new ArrayList<>();
+        parseInput();
     }
 
-    public int partOne() {
-        return 41;
+    public BigInteger partOne() {
+        return equations.stream()
+                .filter(equation -> equation.canBeSolvedWith(List.of(Operator.ADD, Operator.MULTIPLY)))
+                .map(PartialEquation::expectedResult)
+                .reduce(BigInteger.ZERO, BigInteger::add);
     }
 
-    public int partTwo() {
-        return 42;
+    public BigInteger partTwo() {
+        return equations.stream()
+                .filter(equation ->
+                        equation.canBeSolvedWith(List.of(Operator.ADD, Operator.MULTIPLY, Operator.CONCATENATE)))
+                .map(PartialEquation::expectedResult)
+                .reduce(BigInteger.ZERO, BigInteger::add);
+    }
+
+    void parseInput() {
+        input.lines().forEach(line -> {
+            String[] halves = line.split(": ");
+            BigInteger result = new BigInteger(halves[0]);
+            List<BigInteger> operands =
+                    Arrays.stream(halves[1].split(" ")).map(BigInteger::new).toList();
+
+            equations.add(new PartialEquation(result, operands, List.of()));
+        });
     }
 }
--- 2024/src/test/java/org/mellowfish/adventofcode/mmxxiv/DaySevenTest.java
+++ 2024/src/test/java/org/mellowfish/adventofcode/mmxxiv/DaySevenTest.java
@@ -2,12 +2,23 @@ package org.mellowfish.adventofcode.mmxxiv;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 
 class DaySevenTest {
-    static final String SAMPLE_INPUT = """
+    static final String SAMPLE_INPUT =
+            """
+            190: 10 19
+            3267: 81 40 27
+            83: 17 5
+            156: 15 6
+            7290: 6 8 6 15
+            161011: 16 10 13
+            192: 17 8 14
+            21037: 9 7 18 13
+            292: 11 6 16 20
             """;
 
     String fullInput() throws Exception {
@@ -16,21 +27,22 @@ class DaySevenTest {
 
     @Test
     void testPartOneExample() {
-        assertEquals(41, DaySeven.with(SAMPLE_INPUT).partOne());
+        assertEquals(BigInteger.valueOf(3749), DaySeven.with(SAMPLE_INPUT).partOne());
     }
 
     @Test
     void testPartOneFullInput() throws Exception {
-        assertEquals(41, DaySeven.with(fullInput()).partOne());
+        assertEquals(new BigInteger("1289579105366"), DaySeven.with(fullInput()).partOne());
     }
 
     @Test
     void testPartTwoExample() {
-        assertEquals(42, DaySeven.with(SAMPLE_INPUT).partTwo());
+        assertEquals(BigInteger.valueOf(11387), DaySeven.with(SAMPLE_INPUT).partTwo());
     }
 
     @Test
     void testPartTwoWithFullInput() throws Exception {
-        assertEquals(42, DaySeven.with(fullInput()).partTwo());
+        assertEquals(
+                new BigInteger("92148721834692"), DaySeven.with(fullInput()).partTwo());
     }
 }
